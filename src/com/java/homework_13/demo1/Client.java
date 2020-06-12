48a8e3d0a753135062e52ec6274b7e984d411a30
xiaoquan <chensc404@gmail.com>
2020-06-12 09:27:14 +0800

    更新了第12次实验

--- src/com/java/homework_13/demo1/Client.java
+++ src/com/java/homework_13/demo1/Client.java
@@ -1,5 +1,16 @@
 package com.java.homework_13.demo1;
+import java.io.IOException;
 
 public class Client {
+    public static void main(String[] args) throws Exception {
+        methodB();
+    }
 
+    static void methodB() throws Exception {
+        methodA();
+    }
+
+    static void methodA() throws Exception {
+        throw new IOException();
+    }
 }
--- src/com/java/shiyan_12/demoB/Client.java
+++ src/com/java/shiyan_12/demoB/Client.java
@@ -5,14 +5,15 @@ public class Client {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String str = in.nextLine();
+        int number = 0;
         try{
-            int number = Integer.parseInt(str);
+            number = Integer.parseInt(str);
         }catch (NumberFormatException e){
             System.out.print("请输入格式正确的字符串：");
             str = in.nextLine();
+            number = Integer.parseInt(str);
         }finally {
-            System.out.println("success");
+            System.out.println("success:" + number);
         }
-
     }
 }
